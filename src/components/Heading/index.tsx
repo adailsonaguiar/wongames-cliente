57d1c092ecc6513760e2481d8b41755581877e3d
Adailson Aguiar <adailsoacj@live.com>
2020-09-22 00:25:30 -0300

    feat: border on heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -3,10 +3,19 @@ import * as S from './styles'
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
+  lineLeft?: true | false
+  lineBottom?: true | false
 }
 
-const Heading = ({ children, color = 'black' }: HeadingProps) => (
-  <S.Wrapper color={color}>{children}</S.Wrapper>
+const Heading = ({
+  children,
+  color = 'black',
+  lineLeft = false,
+  lineBottom = false
+}: HeadingProps) => (
+  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    {children}
+  </S.Wrapper>
 )
 
 export default Heading
