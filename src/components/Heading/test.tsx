b4dc55b8f899067000bd8134eacb87d2cce3e181
Adailson Aguiar <adailsoacj@live.com>
2020-09-23 23:23:21 -0300

    finally test to Heading

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,13 +1,38 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Heading />)
+  it('should render a white when color is passed', () => {
+    renderWithTheme(<Heading color="white">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      color: '#fafafa'
+    })
+  })
 
-    expect(screen.getByRole('heading', { name: /Heading/i })).toBeInTheDocument()
+  it('should render a black heading by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      color: '#030517'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+
+  it('should render a heading with a line to the bottom side', () => {
+    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
   })
 })
