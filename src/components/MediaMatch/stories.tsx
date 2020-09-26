478842d952d29565961345037ac5f56ee2ec47c6
Adailson Aguiar <adailsoacj@live.com>
2020-09-25 23:04:45 -0300

    testes

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Descktop: Story = () => <MediaMatch>Only on Desktop</MediaMatch>
