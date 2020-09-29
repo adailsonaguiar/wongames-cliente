62640c50e74308616709416dcdf58386eb83d1f2
Adailson Aguiar <adailsoacj@live.com>
2020-09-28 22:28:29 -0300

    add icon in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,15 +4,17 @@ export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: React.ReactNode
 }
 
 const Button = ({
   children,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  icon
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth}>
-    {!!children && <span>{children}</span>}
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+    {icon} {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
 
