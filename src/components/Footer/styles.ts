658b8912f54eb51a5231376ffba721715ce7ca5c
Adailson Aguiar <adailsoacj@live.com>
2020-10-05 21:28:45 -0300

    styles footer component and tests

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,41 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+import * as HeadingStyles from '../Heading/styles'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.footer`
+  ${HeadingStyles.Wrapper} {
+    text-transform: uppercase;
+  }
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: ${theme.grid.gutter};
+    margin-top: ${theme.spacings.medium};
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: repeat(4, 1fr);
+    `}
+  `}
+`
+
+export const Column = styled.div`
+  ${({ theme }) => css`
+    a,
+    span {
+      display: block;
+      color: ${theme.colors.gray};
+      text-decoration: none;
+      margin-bottom: ${theme.spacings.xxsmall};
+      font-size: ${theme.font.sizes.small};
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  `}
+`
+
+export const Copyright = styled.div``
--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
 
 import Footer from '.'
 
 describe('<Footer />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Footer />)
+  it('should render 4 columns', () => {
+    renderWithTheme(<Footer />)
 
-    expect(screen.getByRole('heading', { name: /Footer/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Contact/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /FOLLOW US/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /LOCATION/i })
+    ).toBeInTheDocument()
   })
 })
