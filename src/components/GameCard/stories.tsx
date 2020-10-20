eba954f425e2c726d44fa6b455a7653c2631bf5b
Adailson Aguiar <adailsoacj@live.com>
2020-10-19 23:12:16 -0300

    styles and tests gameCard

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,9 +1,15 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameCard from '.'
+import GameCard, { GameCardProps } from '.'
 
 export default {
   title: 'GameCard',
-  component: GameCard
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstart Games',
+    img: 'img/image 5.png',
+    price: 'R$ 235,00'
+  }
 } as Meta
 
-export const Default: Story = () => <GameCard />
+export const Default: Story<GameCardProps> = (args) => <GameCard {...args} />
--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,13 +1,30 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
 
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstart Games',
+  img: 'img/image 5.png',
+  price: 'R$ 235,00'
+}
+
 describe('<GameCard />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCard />)
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />)
 
-    expect(screen.getByRole('heading', { name: /GameCard/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
   })
 })
