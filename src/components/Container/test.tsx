52c73e474c489817191afa4c603702cf250a3354
Adailson Aguiar <adailsoacj@live.com>
2020-11-03 21:26:22 -0300

    default colors storybook background

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,14 @@
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { Container } from '.'
+
+describe('<Container />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(<Container />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    )
+  })
+})
