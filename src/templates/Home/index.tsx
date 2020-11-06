9b84ea46167920a85666a3fb3fbd06441a1fc816
Adailson Aguiar <adailsoacj@live.com>
2020-11-06 10:45:08 -0300

    feat: seções da home e testes

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -2,11 +2,32 @@ import Footer from 'components/Footer'
 import Menu from 'components/Menu'
 import Container from 'components/Container'
 import * as S from './styles'
+import Heading from 'components/Heading'
 
 const Home = () => (
   <section>
     <Container>
       <Menu />
+    </Container>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        News
+      </Heading>
+    </Container>
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Most popular
+      </Heading>
+    </Container>
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Upcoming
+      </Heading>
+    </Container>
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Free games
+      </Heading>
     </Container>
     <Container>
       <Footer />
