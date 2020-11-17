297242f0ee85afbcf4cce7969807951b1e32e391
Adailson Aguiar <adailsoacj@live.com>
2020-11-16 22:25:10 -0300

    feat em cores das labels

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Checkbox from '.'
@@ -19,5 +20,15 @@ describe('<Checkbox />', () => {
     renderWithTheme(<Checkbox />)
 
     expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with a black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText('checkbox label')).toHaveStyle({
+      color: theme.colors.black
+    })
   })
 })
