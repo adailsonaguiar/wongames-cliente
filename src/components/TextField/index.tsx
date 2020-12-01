371a4e0f81b684a793049c3a361001cff67770bd
Adailson Aguiar <adailsonaguiar@pop-os.localdomain>
2020-11-30 23:18:12 -0300

    add text field component

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,51 @@
+import { InputHTMLAttributes, useState } from 'react'
+import * as S from './styles'
+
+export type TextFieldProps = {
+  onInput?: (value: string) => void
+  label?: string
+  labelFor?: string
+  initialValue?: string
+  icon?: React.ReactNode
+  iconPosition?: 'left' | 'right'
+  disabled?: boolean
+} & InputHTMLAttributes<HTMLInputElement>
+
+const TextField = ({
+  label,
+  labelFor,
+  initialValue = '',
+  onInput,
+  icon,
+  iconPosition = 'left',
+  disabled = false,
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value
+    setValue(newValue)
+
+    !!onInput && onInput(newValue)
+  }
+
+  return (
+    <S.Wrapper disabled={disabled}>
+      {label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      <S.InputWrapper>
+        {icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
+        <S.CustomInput
+          type="text"
+          value={value}
+          onChange={onChange}
+          iconPosition={iconPosition}
+          disabled={disabled}
+          {...props}
+        />
+      </S.InputWrapper>
+    </S.Wrapper>
+  )
+}
+
+export default TextField
