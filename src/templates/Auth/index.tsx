c2bc26bd0658e46868fe88473de72fe8f5c19636
Adailson Aguiar <adailsonaguiar@pop-os.localdomain>
2020-12-10 22:51:08 -0300

    laytous pages sign in/sign up

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -10,19 +10,25 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
-      <Heading color="white">All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
-      <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      <S.BannerContent>
+        <Logo />
+        <div>
+          <Heading color="white">All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
+        <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
     <S.FormContainer>
-      <Logo size="large" color="black" />
-      <Heading lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
-      {children}
+      <S.ContentWrapper>
+        <Logo size="large" color="black" />
+        <Heading lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+        {children}
+      </S.ContentWrapper>
     </S.FormContainer>
   </S.Wrapper>
 )
