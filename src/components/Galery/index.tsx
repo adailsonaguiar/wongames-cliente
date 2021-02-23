7ec18fe33dee0620970f2fa46c80a13c032ae072
adailsonaguiar <adailsonacj@live.com>
2021-02-22 23:36:30 -0300

    finished galery component

--- src/components/Galery/index.tsx
+++ src/components/Galery/index.tsx
@@ -1,17 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
 import { Close } from '@styled-icons/material-outlined/Close'
+import SliderSlick from 'react-slick'
 
 import Slider, { SliderSettings } from 'components/Slider'
 
 import * as S from './styles'
 
-const settings: SliderSettings = {
+const commonSettings: SliderSettings = {
   arrows: true,
-  slidesToShow: 4,
   infinite: false,
   lazyLoad: 'ondemand',
+  nextArrow: <ArrowRight aria-label="next image" />,
+  prevArrow: <ArrowLeft aria-label="previous image" />
+}
+
+const settings: SliderSettings = {
+  ...commonSettings,
+  slidesToShow: 4,
   responsive: [
     {
       breakpoint: 1375,
@@ -37,9 +44,12 @@ const settings: SliderSettings = {
         draggable: true
       }
     }
-  ],
-  nextArrow: <ArrowRight aria-label="next image" />,
-  prevArrow: <ArrowLeft aria-label="previous image" />
+  ]
+}
+
+const modalSettings: SliderSettings = {
+  ...commonSettings,
+  slidesToShow: 1
 }
 
 export type GalleryImageProps = {
@@ -52,6 +62,7 @@ export type GalleryProps = {
 }
 
 const Gallery = ({ items }: GalleryProps) => {
+  const slider = useRef<SliderSlick>(null)
   const [visible, setVisible] = useState(false)
 
   useEffect(() => {
@@ -63,16 +74,20 @@ const Gallery = ({ items }: GalleryProps) => {
 
     return () => window.removeEventListener('keyup', handleKeyUp)
   }, [])
+
   return (
     <S.Wrapper>
-      <Slider settings={settings}>
+      <Slider ref={slider} settings={settings}>
         {items.map((item, index) => (
           <img
             role="button"
             key={`thumb-${index}`}
             src={item.src}
             alt={`Thumb - ${item.label}`}
-            onClick={() => setVisible(true)}
+            onClick={() => {
+              setVisible(true)
+              slider.current!.slickGoTo(index, true)
+            }}
           />
         ))}
       </Slider>
@@ -84,6 +99,17 @@ const Gallery = ({ items }: GalleryProps) => {
         >
           <Close size={40} />
         </S.BtnClose>
+        <S.Content>
+          <Slider ref={slider} settings={modalSettings}>
+            {items.map((item, index) => (
+              <img
+                key={`gallery-${index}`}
+                src={item.src}
+                alt={`${item.label}`}
+              />
+            ))}
+          </Slider>
+        </S.Content>
       </S.Modal>
     </S.Wrapper>
   )
