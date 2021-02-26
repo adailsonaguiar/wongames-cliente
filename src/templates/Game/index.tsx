8bc6a2a93f7bff87b45a2bdd1a945c76b6d6f5c3
adailsonaguiar <adailsonacj@live.com>
2021-02-25 21:20:41 -0300

    feat: zindex of menu styles

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,9 +1,12 @@
 import Base from 'templates/Base'
-// import * as S from './styles'
+import * as S from './styles'
 
 const Game = () => (
   <Base>
-    <h1>Game</h1>
+    <S.Cover
+      src="https://blog.bestbuy.ca/wp-content/uploads/2020/06/Cyberpunk-2077-banner.png"
+      aria-label="cover"
+    />
   </Base>
 )
 
--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -1,3 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.main``
+
+type CoverProps = {
+  src: string
+}
+
+export const Cover = styled.div<CoverProps>`
+  ${({ src }) => css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 39.5rem;
+    background-image: url(${src});
+    background-size: cover;
+    background-position: top center;
+    opacity: 0.4;
+  `}
+`
