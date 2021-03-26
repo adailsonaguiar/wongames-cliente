f81fde66b5b13e90bb13ebf778a0257bb1ffbf66
adailsonaguiar <adailsonacj@live.com>
2021-03-25 21:53:12 -0300

    create tests

---
+++ src/templates/Wishlist/test.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+  recommendedGames: gamesMock,
+  recommendedHighlit: highlightMock
+}
+
+import Wishlist from '.'
+
+jest.mock('components/ShowCase', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Showcase"></div>
+    }
+  }
+})
+
+describe('<Wishlist />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Wishlist {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /wishlist/i })
+    ).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
+  })
+})
