413dd44f95e18066ef65d751ede623419155fe56
adailsonaguiar <adailsonacj@live.com>
2021-04-28 23:13:42 -0300

    create a ProfileMenu component

--- src/components/ProfileMenu/stories.tsx
+++ src/components/ProfileMenu/stories.tsx
@@ -3,7 +3,12 @@ import ProfileMenu from '.'
 
 export default {
   title: 'ProfileMenu',
-  component: ProfileMenu
+  component: ProfileMenu,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
 export const Default: Story = () => <ProfileMenu />
--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -1,3 +1,50 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Nav = styled.nav`
+  ${({ theme }) => css`
+    display: flex;
+    border-bottom: 0.1rem solid ${theme.colors.lightGray};
+
+    ${media.greaterThan('medium')`
+       flex-direction: column;
+       border: 0;
+
+       a:not(:last-child){
+           border-bottom: 0.1rem solid ${theme.colors.lightGray}
+       }
+    `}
+  `}
+`
+
+export const Link = styled.a`
+  ${({ theme }) => css`
+    color: ${theme.colors.black};
+    background-color: ${theme.colors.white};
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    padding: ${theme.spacings.xsmall} 0;
+    text-decoration: none;
+    transition: background, color, ${theme.transition.default};
+
+    span {
+      display: none;
+    }
+
+    &:hover {
+      color: ${theme.colors.white};
+      background: ${theme.colors.primary};
+    }
+
+    ${media.greaterThan('medium')`
+        justify-content: flex-start;
+        padding-left: ${theme.spacings.xsmall};
+        padding-right: ${theme.spacings.xsmall};
+        span {
+            display: block;
+            margin-left: ${theme.spacings.xsmall}
+        }
+    `}
+  `}
+`
